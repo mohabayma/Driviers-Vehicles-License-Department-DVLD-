5a72e9d5d94913e161dc436816c16d4dada99e78
Mohab Ayman <145718919+mohabayma@users.noreply.github.com>
2025-08-31 20:58:44 +0300

    Add files via upload

---
+++ DVLD - Data Access Layer/clsConnString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD___Data_Access_Layer
+{
+    internal class clsConnString
+    {
+        public static string ConnSting= "Server=.;Database=DVLD;Integrated Security=True;";
+    }
+}
